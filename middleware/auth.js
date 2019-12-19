e65db2e61131dcbe88cf1b726d5c11640e293ab4
João Victor de Alencar <joaovictoralencarpascoal@gmail.com>
2019-12-19 18:57:46 -0300

    mais uma vez arrumando erros no auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,5 @@
-export default function ({ store, error }) {
+export default function ({ store, route, redirect }) {
     if (!store.state.authUser) {//se não existir no store algum usuário autenticado, lança o erro
-        return error({
-            message: 'You are not connected',
-            statusCode: 403
-        })
+        return redirect('/login')
     }
 }
