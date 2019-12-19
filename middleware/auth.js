5f58d1b6d7a605f7f24f33bf202f5c60052c1468
João Victor de Alencar <joaovictoralencarpascoal@gmail.com>
2019-12-19 16:17:07 -0300

    consertando middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,5 @@
 export default function ({ store, error }) {
-    if (store.state.authUser) {//se não existir no store algum usuário autenticado, lança o erro
-        console.log(error)
+    if (!store.state.authUser) {//se não existir no store algum usuário autenticado, lança o erro
         return error({
             message: 'You are not connected',
             statusCode: 403
