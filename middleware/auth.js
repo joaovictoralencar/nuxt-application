76a191368bb97547ea1e28f632436b1b5832a328
João Victor de Alencar <joaovictoralencarpascoal@gmail.com>
2019-12-23 11:49:37 -0300

    TESTANDO MT COISA AINDA

--- middleware/auth.js
+++
@@ -1,8 +0,0 @@
-export default function ({ store, error }) {
-    if (!store.state.authUser) {//se não existir no store algum usuário autenticado, lança o erro
-        error({
-            message: 'You are not connected',
-            statusCode: 403
-        })
-    }
-}
